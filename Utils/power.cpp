85a70386639f63876d88ab3128f696f88e25d245
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-24 08:49:11 +0300

    Add powerDown functions with watchdog wakeup

--- Utils/power.cpp
+++ Utils/power.cpp
@@ -1,8 +1,32 @@
 #include <Arduino.h>
 #include <avr/sleep.h>
 #include <util/atomic.h>
+#include "power.h"
+
+#ifndef CUSTOM_WATCHDOG_INT_HANDLER
+ISR(WDT_vect) { [](){}; }  // Set default watchdog interrupt handler to do nothing. #define CUSTOM_WATCHDOG_INT_HANDLER to use other implementation
+#endif
+
+inline void setWDTCSR(byte wdtcsr) {
+  ATOMIC_BLOCK(ATOMIC_FORCEON) {
+      WDTCSR |= bit(WDE) | bit(WDCE); // Timed sequence, set WDE & WDCE to allow changing WDTCSR
+      WDTCSR = wdtcsr;
+  }
+}
+
+void enableWDTInterrupts(WatchdogTimerPrescaler prescaler) {
+    byte newWDTCSR = (WDTCSR & ~0x27) | (prescaler & 0x27);  // Set prescaler, 0x27 is mask for bits 5 & 2..0. First clear them to 0, then set if set in prescaler.
+    newWDTCSR |= bit(WDIE);             // Enable watchdog interrupts
+    MCUSR &= ~ bit(WDRF);               // Clear Watchdog System Reset Flag
+    setWDTCSR(newWDTCSR);
+}
+
+void disableWDTInterrupts() {
+  setWDTCSR(WDTCSR & ~ bit(WDIE));
+}
 
 void powerDown() {
+  byte adcsraSave = ADCSRA;
   ADCSRA &= ~ bit(ADEN); // disable the ADC
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   ATOMIC_BLOCK(ATOMIC_FORCEON) {
@@ -14,6 +38,25 @@ void powerDown() {
   }
 
   sleep_cpu();
-
   sleep_disable();
+  ADCSRA = adcsraSave;
+}
+
+void powerDown(WatchdogTimerPrescaler prescaler) {
+  if(prescaler != WAKEUP_DELAY_NO_SLEEP) {
+    enableWDTInterrupts(prescaler);
+    powerDown();
+    disableWDTInterrupts();
+  }
+}
+
+void powerDown(WatchdogTimerPrescaler prescaler1, WatchdogTimerPrescaler prescaler2) {
+  powerDown(prescaler1);
+  powerDown(prescaler2);
+}
+
+void powerDown(WatchdogTimerPrescaler prescaler1, WatchdogTimerPrescaler prescaler2, WatchdogTimerPrescaler prescaler3) {
+  powerDown(prescaler1);
+  powerDown(prescaler2);
+  powerDown(prescaler3);
 }
--- Utils/power.h
+++ Utils/power.h
@@ -1,6 +1,23 @@
 #ifndef CHACAL_UTILS_POWER_H
 #define CHACAL_UTILS_POWER_H
 
+enum WatchdogTimerPrescaler {
+  WAKEUP_DELAY_NO_SLEEP = -1,
+  WAKEUP_DELAY_16_MS = 0x0,
+  WAKEUP_DELAY_32_MS = 0x1,
+  WAKEUP_DELAY_64_MS = 0x2,
+  WAKEUP_DELAY_125_MS = 0x3,
+  WAKEUP_DELAY_250_MS = 0x4,
+  WAKEUP_DELAY_500_MS = 0x5,
+  WAKEUP_DELAY_1_S = 0x6,
+  WAKEUP_DELAY_2_S = 0x7,
+  WAKEUP_DELAY_4_S = 0x20,  // WDP3 bit is actually bit 5, see datasheet 10.9.2
+  WAKEUP_DELAY_8_S = 0x21,  // WDP3 bit is actually bit 5, see datasheet 10.9.2
+};
+
 void powerDown();
+void powerDown(WatchdogTimerPrescaler prescaler);
+void powerDown(WatchdogTimerPrescaler prescaler1, WatchdogTimerPrescaler prescaler2);
+void powerDown(WatchdogTimerPrescaler prescaler1, WatchdogTimerPrescaler prescaler2, WatchdogTimerPrescaler prescaler3);
 
 #endif  //CHACAL_UTILS_POWER_H
