1a884ff1bb6a8c1321b0b269bf1923498b8e3d91
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-06-12 23:23:55 +0300

    Add support for Attiny devices
    Atmegas have WDTCSR register, but Attinys use WDTCR. The relevant bits
    are same however, only name is different.

--- Utils/power.cpp
+++ Utils/power.cpp
@@ -7,22 +7,28 @@
 ISR(WDT_vect) { [](){}; }  // Set default watchdog interrupt handler to do nothing. #define CUSTOM_WATCHDOG_INT_HANDLER to use other implementation
 #endif
 
-inline void setWDTCSR(byte wdtcsr) {
+#if defined(WDTCSR)       // Atmega family has WDTCSR
+#define WDT_REG WDTCSR
+#elif defined(WDTCR)      // Attiny family has WDTCR
+#define WDT_REG WDTCR
+#endif
+
+inline void setWDT_REG(byte wdt_reg) {
   ATOMIC_BLOCK(ATOMIC_FORCEON) {
-      WDTCSR |= bit(WDE) | bit(WDCE); // Timed sequence, set WDE & WDCE to allow changing WDTCSR
-      WDTCSR = wdtcsr;
+      WDT_REG |= bit(WDE) | bit(WDCE); // Timed sequence, set WDE & WDCE to allow changing WDTCSR/WDTCR
+      WDT_REG = wdt_reg;
   }
 }
 
 void enableWDTInterrupts(WatchdogTimerPrescaler prescaler) {
-    byte newWDTCSR = (WDTCSR & ~0x27) | (prescaler & 0x27);  // Set prescaler, 0x27 is mask for bits 5 & 2..0. First clear them to 0, then set if set in prescaler.
-    newWDTCSR |= bit(WDIE);             // Enable watchdog interrupts
-    MCUSR &= ~ bit(WDRF);               // Clear Watchdog System Reset Flag
-    setWDTCSR(newWDTCSR);
+    byte newWDT_REG = (WDT_REG & ~0x27) | (prescaler & 0x27);  // Set prescaler, 0x27 is mask for bits 5 & 2..0. First clear them to 0, then set if set in prescaler.
+    newWDT_REG |= bit(WDIE);             // Enable watchdog interrupts
+    MCUSR &= ~ bit(WDRF);                // Clear Watchdog System Reset Flag
+    setWDT_REG(newWDT_REG);
 }
 
 void disableWDTInterrupts() {
-  setWDTCSR(WDTCSR & ~ bit(WDIE));
+  setWDT_REG(WDT_REG & ~ bit(WDIE));
 }
 
 void powerDown() {
